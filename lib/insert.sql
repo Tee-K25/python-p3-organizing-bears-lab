b5d5ca859f6bfe1557f4560ff725f090b6881a53
Tony <tony.kiplagat@student.moringaschool.com>
2023-09-24 16:15:52 +0300

    Zoo inventory

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'M', 'Brown', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'F', 'Black', 'Energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'White', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 16, 'M', 'Brown', 'Grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'Blonde', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'Green', 'Mischievous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'Gray', 'Shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'Rainbow', 'Mysterious', 0);
+
